792d4d7285354570062a7d84b3bc656825aba75c
ibaialberdi <ibaialberdi@gmail.com>
2024-04-28 00:35:55 +0200

    graps work

---
+++ src/lib/shared/formatters.ts
@@ -0,0 +1,16 @@
+// 2000 => 2K
+export const formatLargeNumber = (value: number): string => {
+  return Intl.NumberFormat("es-ES", {
+    notation: "compact",
+    maximumFractionDigits: 1,
+  }).format(value)
+}
+
+export function formatCurrency(amount: number) {
+  const formatter = new Intl.NumberFormat("es-ES", {
+    style: "currency",
+    currency: "EUR",
+  })
+
+  return formatter.format(amount)
+}
